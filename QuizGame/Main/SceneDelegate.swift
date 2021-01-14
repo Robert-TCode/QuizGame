beff7f88f851c4b9460d01c327cf37beeafb70f1
Robert Tanase <robert.tanase97@gmail.com>
2021-01-14 22:28:53 +0000

    Compose all elements together to start the app

--- QuizGame/Main/SceneDelegate.swift
+++ QuizGame/Main/SceneDelegate.swift
@@ -1,55 +1,60 @@
 //  Created by TCode on 30/12/2020.
 
 import UIKit
+import QEngine
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    var game: Game<Question<String>, [String], NavigationControllerRouter>?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
-//        let questionVC = QuestionViewController(question: "Question?", options: ["Option 1", "Option 2"]) {
-//            print("Selection \($0)")
-//        }
-//        questionVC.tableView.allowsMultipleSelection = true
-
-        let resultsVC = ResultsViewController(summary: "You've got 1/2 correct", answers: [PresentableAnswer(question: "Question 1", answer: "Answer", wrongAnswer: nil), PresentableAnswer(question: "Question 2", answer: "Correct answer", wrongAnswer: "Wrong answer")])
+        let navigationController = UINavigationController()
 
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = resultsVC
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
+
+        let database = createData()
+        let factory = iOSViewControllerFactory(questions: database.questions, options: database.options, correctAnswers: database.correctAnswers)
+        let router = NavigationControllerRouter(navigationController, factory: factory)
+
+        game = startGame(questions: database.questions, router: router, correctAnswers: database.correctAnswers)
     }
 
-    func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+    private func createData() -> (questions: [Question<String>], options: [Question<String>: [String]], correctAnswers: [Question<String>: [String]]) {
+        let question1 = Question.singleAnswer("Who said \"Winter is coming\" for the first time in Game of Thrones?")
+        let option1 = "Maester Luwin"
+        let option2 = "John Snow"
+        let option3 = "Robb Stark"
+        let option4 = "Eddard Stark"
+
+
+        let question2 = Question.multipleAnswer("Who went beyond the Wall in Game of Thrones?")
+        let option5 = "Randyll Tarly"
+        let option6 = "Tyrion Lannister"
+        let option7 = "Meera Reed"
+        let option8 = "Jorah Mormont"
+
+        let options = [question1: [option1, option2, option3, option4],
+                       question2: [option5, option6, option7, option8]]
+
+        let questions = [question1, question2]
+        let correctAnswers = [question1: [option4], question2: [option7, option8]]
+
+        return (questions, options, correctAnswers)
     }
 
-    func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-    }
+    func sceneDidDisconnect(_ scene: UIScene) { }
 
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
+    func sceneDidBecomeActive(_ scene: UIScene) { }
 
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
-    }
+    func sceneWillResignActive(_ scene: UIScene) { }
 
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
-    }
+    func sceneWillEnterForeground(_ scene: UIScene) { }
+
+    func sceneDidEnterBackground(_ scene: UIScene) { }
 }
 
